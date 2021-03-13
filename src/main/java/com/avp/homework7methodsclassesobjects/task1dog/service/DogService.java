b318c891b72a8fbf2680cfb71acd8fafc53728bb
VladimirPetranovski <desantluninec@mail.ru>
2021-03-13 21:04:38 +0300

    I solved hw7 classes Dog

---
+++ src/main/java/com/avp/homework7methodsclassesobjects/task1dog/service/DogService.java
@@ -0,0 +1,14 @@
+package com.avp.homework7methodsclassesobjects.task1dog.service;
+
+public class DogService {
+
+    String voice() {
+        return "gav-gav";
+    }
+    String eat() {
+        return "Ням-ням";
+    }
+    String sleep() {
+        return "Thrrrr";
+    }
+}

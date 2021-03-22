ab112e4381208c0a78f8bb4a0c38e86cd447da2e
VladimirPetranovski <desantluninec@mail.ru>
2021-03-22 21:24:03 +0300

    I've removed the extra variable

--- src/main/java/com/avp/homework5_loops/task3_numberService/service/NumberService.java
+++ src/main/java/com/avp/homework5_loops/task3_numberService/service/NumberService.java
@@ -3,16 +3,15 @@ package com.avp.homework5_loops.task3_numberService.service;
 public class NumberService {
     public int rangeSum(int start, int finish) {
         int sum = 0;
-        int i = start;
         if (start < finish) {
-            while (i <= finish) {
-                sum += i;
-                i++;
+            while (start <= finish) {
+                sum += start;
+                start++;
             }
         } else {
-            while (i >= finish) {
-                sum += i;
-                i--;
+            while (start >= finish) {
+                sum += start;
+                start--;
             }
         }
         return sum;
@@ -20,20 +19,19 @@ public class NumberService {
 
     public int rangeEvenCount(int start, int finish) {
         int sum = 0;
-        int i = start;
         if (start < finish) {
-            while (i <= finish) {
-                if (i % 2 == 0) {
-                    sum += i;
+            while (start <= finish) {
+                if (start % 2 == 0) {
+                    sum += start;
                 }
-                i++;
+                start++;
             }
         } else {
-            while (i >= finish) {
-                if (i % 2 == 0) {
-                    sum += i;
+            while (start >= finish) {
+                if (start % 2 == 0) {
+                    sum += start;
                 }
-                i--;
+                start--;
             }
         }
         return sum;
